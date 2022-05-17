1e2d675112f85105fe4db62ac84fd528e0ffb798
davidevecchi <davide.vecchi.64@gmail.com>
2022-05-17 10:32:41 +0200

    fixes

--- app/src/main/java/com/plantalot/animations/AppbarCollapsingBehavior.java
+++ app/src/main/java/com/plantalot/animations/AppbarCollapsingBehavior.java
@@ -24,6 +24,10 @@ public class AppbarCollapsingBehavior extends CoordinatorLayout.Behavior<View> {
 	private int[] mView;
 	private int[] mTarget;
 	
+	private boolean isSetUp = false;
+
+//	private float factorPrev = 0f;
+	
 	private final Context context;
 	
 	public AppbarCollapsingBehavior(Context context, AttributeSet attrs, int[] collapsing, int collapsed) {
@@ -47,19 +51,57 @@ public class AppbarCollapsingBehavior extends CoordinatorLayout.Behavior<View> {
 	// FIXME animation not completed
 	@Override
 	public boolean onDependentViewChanged(@NonNull CoordinatorLayout parent, @NonNull View child, @NonNull View dependency) {
-		setup(parent, child);
+		if (!isSetUp) {
+			setup(parent, child);
+			isSetUp = true;
+		}
 		AppBarLayout appBarLayout = (AppBarLayout) dependency;
 		
 		int range = appBarLayout.getTotalScrollRange();
-		float factor = -appBarLayout.getY() / range + 0.05f;  // fixme epsilon
+		float factor = -appBarLayout.getY() / (float) range;
 		
-		System.out.println(factor);
+		
+		CoordinatorLayout.LayoutParams lp = (CoordinatorLayout.LayoutParams) child.getLayoutParams();
+
+//		if (factor < 0.5) {
+//			factor = 0;
+//		}
+//		if (factor > 0.5) {
+//			factor = 1;
+//		}
+
+//		int left;
+//		int top;
+//		int width;
+//		int height;
+//		float epsilon = 0.005f;
+
+//		if (Math.abs(factor - factorPrev) > 0.5f) {
+//			if (factor > factorPrev) {
+//				left = mTarget[X];
+//				top = mTarget[Y];
+//				width = mTarget[WIDTH];
+//				height = mTarget[HEIGHT];
+//				System.out.println("---------------");
+//			} else {
+//				left = mView[X];
+//				top = mView[Y];
+//				width = mView[WIDTH];
+//				height = mView[HEIGHT];
+//				System.out.println("+++++++++++++++");
+//			}
+//		} else {
+		
 		int left = mView[X] + (int) (factor * (mTarget[X] - mView[X]));
 		int top = mView[Y] + (int) (factor * (mTarget[Y] - mView[Y]));
 		int width = mView[WIDTH] + (int) (factor * (mTarget[WIDTH] - mView[WIDTH]));
 		int height = mView[HEIGHT] + (int) (factor * (mTarget[HEIGHT] - mView[HEIGHT]));
 		
-		CoordinatorLayout.LayoutParams lp = (CoordinatorLayout.LayoutParams) child.getLayoutParams();
+		System.out.println("" + left);
+		
+//		}
+//		factorPrev = factor;
+		
 		lp.width = width;
 		lp.height = height;
 		child.setLayoutParams(lp);
@@ -69,18 +111,12 @@ public class AppbarCollapsingBehavior extends CoordinatorLayout.Behavior<View> {
 		appBarLayout.setBackgroundColor(ColorUtils.attrColor(
 				com.google.android.material.R.attr.colorPrimary,
 				context,
-				6 + (int) (12 * factor)  // fixme
+				5 + (int) (11 * factor)
 		));
 		
+		float alphaOffset = 0.5f;
 		TextView subtitle = parent.findViewById(R.id.ortaggio_fl_appbar_subtitle);
-		subtitle.setAlpha((float) Math.max(0, (factor - 0.25)*1.32));  // fixme
-		
-//		TextView subtitle2 = parent.findViewById(R.id.ortaggio_fl_toolbar_subtitle2);
-//		subtitle2.setAlpha((float) Math.max(0, height - MIN_H - (MAX_H - MIN_H) / 2) / (float) ((MAX_H - MIN_H) / 2));
-//		subtitle1.setText("h: " + MAX_H);
-
-//		View dropdown = parent.findViewById(R.id.ortaggio_fl_dropdown);
-//		dropdown.setElevation(Utils.dp2px(height == mTarget[HEIGHT] ? 4 : 0, context));
+		subtitle.setAlpha((float) Math.max(0, (factor - alphaOffset) * (1f / (1f - alphaOffset))));
 		
 		return true;
 	}
@@ -101,8 +137,8 @@ public class AppbarCollapsingBehavior extends CoordinatorLayout.Behavior<View> {
 			throw new IllegalStateException("target view not found");
 		}
 		
-		mTarget[WIDTH] += target.getWidth();
-		mTarget[HEIGHT] += target.getHeight();
+		mTarget[WIDTH] = target.getWidth();
+		mTarget[HEIGHT] = target.getHeight();
 		
 		View view = target;
 		while (view != parent) {
