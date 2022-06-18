3488b7488d3c086332a04fb9e8949d72c4a36c3b
Itakello <maxste000@gmail.com>
2022-06-18 19:34:25 +0200

    Add anonymous login at start

--- app/src/main/java/com/plantalot/activities/MainActivity.java
+++ app/src/main/java/com/plantalot/activities/MainActivity.java
@@ -4,19 +4,25 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.plantalot.R;
 import com.plantalot.database.DbStrings;
 
 public class MainActivity extends AppCompatActivity {//implements NavigationHost {
-
+	private static final String TAG = "MainActivity";
 	private FirebaseAuth mAuth;
 
 	@Override
@@ -32,25 +38,31 @@ public class MainActivity extends AppCompatActivity {//implements NavigationHost
 		super.onStart();
 		// Check if user is signed in (non-null) and update UI accordingly.
 		FirebaseUser currentUser = mAuth.getCurrentUser();
+		signInAnonymously();
 	}
-	
-	/**
-	 * Navigate to the given fragment.
-	 *
-	 * @param fragment       Fragment to navigate to.
-	 * @param addToBackstack Whether or not the current fragment should be added to the backstack.
-	 */
-//	@Override
-//	public void navigateTo(Fragment fragment, boolean addToBackstack) {
-//		FragmentTransaction transaction =
-//				getSupportFragmentManager()
-//						.beginTransaction()
-//						.replace(R.id.container, fragment);
-//
-//		if (addToBackstack) {
-//			transaction.addToBackStack(null);
-//		}
-//
-//		transaction.commit();
-//	}
+
+	private void signInAnonymously() {
+//		showProgressBar();
+		mAuth.signInAnonymously()
+				.addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+					@Override
+					public void onComplete(@NonNull Task<AuthResult> task) {
+						if (task.isSuccessful()) {
+							// Sign in success, update UI with the signed-in user's information
+							Log.d(TAG, "signInAnonymously:success");
+							FirebaseUser user = mAuth.getCurrentUser();
+//							updateUI(user);
+						} else {
+							// If sign in fails, display a message to the user.
+							Log.w(TAG, "signInAnonymously:failure", task.getException());
+							Toast.makeText(getApplicationContext(), "Authentication failed.",
+									Toast.LENGTH_SHORT).show();
+//							updateUI(null);
+						}
+
+//						hideProgressBar();
+					}
+				});
+	}
+
 }
