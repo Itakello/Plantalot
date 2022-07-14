817f1e9d581a19a04c5def4aca2ef59e5430c88a
davidevecchi <davide.vecchi.64@gmail.com>
2022-07-14 16:45:55 +0200

    added splash screen & changed icon

---
+++ app/src/main/java/com/plantalot/activities/Splash.java
@@ -0,0 +1,31 @@
+package com.plantalot.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.RequiresApi;
+
+import com.plantalot.R;
+import com.plantalot.database.Db;
+
+public class Splash extends Activity {
+	
+	private final static int SPLASH_DISPLAY_LENGTH = 1600;
+	
+	@RequiresApi(api = Build.VERSION_CODES.N)
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash);
+		Db.init(this);
+		
+		new Handler().postDelayed(() -> {
+			Intent mainIntent = new Intent(this, MainActivity.class);
+			startActivity(mainIntent);
+			finish();
+		}, SPLASH_DISPLAY_LENGTH);
+	}
+}
