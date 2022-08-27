592c339b83c21c0da0aeab8ed011705fafc964c6
davidevecchi <davide.vecchi.64@gmail.com>
2022-08-27 17:57:51 +0200

    navigation bug fixed

--- app/src/main/java/com/plantalot/fragments/AllPlantsFragment.java
+++ app/src/main/java/com/plantalot/fragments/AllPlantsFragment.java
@@ -491,7 +491,7 @@ public class AllPlantsFragment extends Fragment {
 	private void setupToolbar() {
 		AppCompatActivity activity = (AppCompatActivity) getActivity();
 		if (activity != null) activity.setSupportActionBar(toolbar);
-		toolbar.setNavigationOnClickListener(view -> Navigation.findNavController(view).navigate(R.id.action_goto_home));
+		toolbar.setNavigationOnClickListener(v -> Navigation.findNavController(view).navigate(R.id.action_goto_home));
 	}
 	
 	@RequiresApi(api = Build.VERSION_CODES.N)
